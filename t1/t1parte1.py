6135fb1aaec44b369b1ed51a98beeb35c9070d3c
Joao Bolsson <joaovictorbolsson@gmail.com>
2017-03-12 21:28:48 -0300

    Definindo hasEqHeads

--- t1/t1parte1.py
+++ t1/t1parte1.py
@@ -1,10 +1,29 @@
 # coding=utf-8
 """
-Defina uma função somaQuad(x,y) que calcule a soma dos quadrados de dois números x e y.
+Parte 1 do Trabalho 1 - Paradigmas de Programação (UFSM)
+
+@author João Bolsson (jvmarques@inf.ufsm.br)
+@since 2017, 12 Mar.
 """
+
+'''
+Defina uma função somaQuad(x,y) que calcule a soma dos quadrados de dois números x e y.
+'''
 
 
 def somaQuad(x, y):
     quad_x = x * x
     quad_y = y * y
     return quad_x + quad_y
+
+
+'''
+Crie uma função hasEqHeads(l1,l2) que verifique se as listas l1 e l2 possuem o mesmo primeiro elemento.
+'''
+
+
+def hasEqHeads(l1, l2):
+    assert isinstance(l1, list)
+    assert isinstance(l2, list)
+
+    return l1[0] == l2[0]
