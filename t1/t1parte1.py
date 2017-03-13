d3ceb2c03f5b47d78d318bb88d8be95ea17a951e
Joao Bolsson <joaovictorbolsson@gmail.com>
2017-03-13 16:55:02 -0300

    definindo funcoes de 5 a 8

--- t1/t1parte1.py
+++ t1/t1parte1.py
@@ -7,7 +7,7 @@ Parte 1 do Trabalho 1 - Paradigmas de Programação (UFSM)
 """
 
 '''
-Defina uma função somaQuad(x,y) que calcule a soma dos quadrados de dois números x e y.
+1) Defina uma função somaQuad(x,y) que calcule a soma dos quadrados de dois números x e y.
 '''
 
 
@@ -18,7 +18,7 @@ def somaQuad(x, y):
 
 
 '''
-Crie uma função hasEqHeads(l1,l2) que verifique se as listas l1 e l2 possuem o mesmo primeiro elemento.
+2) Crie uma função hasEqHeads(l1,l2) que verifique se as listas l1 e l2 possuem o mesmo primeiro elemento.
 '''
 
 
@@ -29,7 +29,7 @@ def hasEqHeads(l1, l2):
     return l1[0] == l2[0]
 
 '''
-Escreva uma função que receba uma lista de nomes e retorne uma lista com a string "Sr. " adicionada ao início de
+3) Escreva uma função que receba uma lista de nomes e retorne uma lista com a string "Sr. " adicionada ao início de
 cada nome. Defina uma função auxiliar para ajudar neste exercício.
 '''
 
@@ -40,7 +40,7 @@ def nomeia(nomes):
     return list(map(lambda x: "Sr. " + str(x), nomes))
 
 '''
-Crie uma função que receba uma string e retorne o número de espaços nela contidos.
+4) Crie uma função que receba uma string e retorne o número de espaços nela contidos.
 Defina uma função auxiliar para ajudar neste exercício.
 '''
 
@@ -48,3 +48,46 @@ Defina uma função auxiliar para ajudar neste exercício.
 def countSpaces(string):
     assert isinstance(string, str)
     return len(list(filter(lambda x: x == ' ', string)))
+
+
+'''
+5) Escreva uma função que, dada uma lista de números, calcule 3n*2 + 2/n + 1 para cada número n da lista.
+Defina uma função auxiliar para ajudar neste exercício.
+'''
+
+
+def applyFormula(numbers):
+    assert isinstance(numbers, list)
+    return list(map(lambda x: (3*x*2) + (2/x) + 1, numbers))
+
+'''
+6) Escreva uma função que, dada uma lista de números, retorne uma lista com apenas os que forem negativos.
+Defina uma função auxiliar para ajudar neste exercício.
+'''
+
+
+def filterNegatives(numbers):
+    assert isinstance(numbers, list)
+    return list(filter(lambda x: x < 0, numbers))
+
+
+'''
+7) Escreva uma função que receba uma lista de números e retorne somente os que estiverem entre 1 e 100, inclusive.
+Defina uma função auxiliar para ajudar neste exercício.
+'''
+
+
+def filterLessThan100(numbers):
+    assert isinstance(numbers, list)
+    return list(filter(lambda x: (1 < x < 100), numbers))
+
+
+'''
+8) Escreva uma função que receba uma lista de números e retorne somente aqueles que forem pares.
+Defina uma função auxiliar para ajudar neste exercício.
+'''
+
+
+def filterEven(numbers):
+    assert isinstance(numbers, list)
+    return list(filter(lambda x: x % 2 == 0, numbers))
