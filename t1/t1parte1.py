c309e7f9c212f3e8411555f3474f32bf38d08470
Joao Bolsson <joaovictorbolsson@gmail.com>
2017-03-12 21:04:30 -0300

    iniciando t1 (funcao somaQuad)

---
+++ t1/t1parte1.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+"""
+Defina uma função somaQuad(x,y) que calcule a soma dos quadrados de dois números x e y.
+"""
+
+
+def somaQuad(x, y):
+    quad_x = x * x
+    quad_y = y * y
+    return quad_x + quad_y
