0bb8fc6e8b37051d3b53b8a36899c5c443110167
Joao Bolsson <joaovictorbolsson@gmail.com>
2017-03-25 16:36:37 -0300

    cria função para gerar cores aleatórias

--- t2/t2parte2.py
+++ t2/t2parte2.py
@@ -24,6 +24,12 @@ def applyStyles(rects, styles):
     return list(zip(rects, itertools.cycle(styles)))
 
 
+'''
+1) Reimplemente a função genRects para gerar uma lista com o número correto de retângulos, nas posições corretas,
+usando os argumentos n, w e h. A figura produzida deverá ser igual a colors.svg. Use list comprehension.
+'''
+
+
 def genRects(n, w, h):
     return [((x * w, 0.0), w, h) for x in range(0, n)]
 
@@ -34,11 +40,28 @@ def writeFile(fname, contents):
     f.close()
 
 
+'''
+2) Note que a lista styles, na função main(), contém strings que especificam as cores dos retângulos.
+Essa lista é definida com strings fixas (não é gerada por uma função). Crie uma função para gerar essa lista de cores,
+tendo no mínimo um parâmetro que defina a quantidade (n) de cores a serem geradas. Você pode usar qualquer esquema de
+geração de cores, por exemplo produzindo n variações de vermelho, etc. Use sua criatividade :-)
+
+Função que gera n cores aleatórias
+'''
+
+
+def createColors(n):
+    from random import randint
+    return [("fill:rgb(" + str(randint(0, 255)) + "," + str(randint(0, 255)) + "," + str(randint(0, 255)) + ")") for x
+            in
+            range(0, n)]
+
+
 def main():
     maxWidth = 1000
     maxHeight = 100
     rects = genRects(10, 50, 50)
-    styles = ["fill:rgb(140,0,0)", "fill:rgb(0,140,0)"]
+    styles = createColors(10)
     rectstyles = applyStyles(rects, styles)
     writeFile("mycolors.svg", svgImage(maxWidth, maxHeight, rectstyles))
 
