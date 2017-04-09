88b936f224b69c326d6e6d3a2afbc3e92198a0f8
Joao Bolsson <joaovictorbolsson@gmail.com>
2017-04-09 00:41:20 -0300

    questao 6 parte 2

--- t3/t3parte2.pl
+++ t3/t3parte2.pl
@@ -28,3 +28,20 @@ has5(L) :- list_length(L, X), X = 5.
 %lista de N elementos. Agora você pode usar um predicado pré-definido.
 
 hasN(L, N) :- length(L, X), X = N.
+
+%Defina um predicado isBin(L) que seja verdadeiro se L for uma 
+%lista contendo somente elementos 0 e 1. Use recursão.
+
+foundBinaries([], 0).
+foundBinaries([H|X], L) :-
+	(H = 0; H = 1),
+	foundBinaries(X, N),
+	L is N+1.
+
+isBin(L) :- foundBinaries(L, X), length(L, Y), X = Y.
+
+%Defina um predicado mesmaPosicao(A,L1,L2) para verificar se um 
+%elemento A está na mesma posição nas listas L1 e L2. Assuma que 
+%L1 e L2 sejam permutações de uma lista de elementos distintos, 
+%sem repetições. Dica: procure auxílio em predicados pré-definidos.
+
