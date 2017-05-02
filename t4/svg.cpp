53e774cff3995fbfb38c6cfe14b754c7cbd4f14b
Joao Bolsson <joaovictorbolsson@gmail.com>
2017-05-02 00:09:25 -0300

    finalizando (incompleto)

--- t4/svg.cpp
+++ t4/svg.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-#define NUM_CIRCLES 10
+#define NUM_CIRCLES 6
 #define RAD 20
 
 int main() {
@@ -17,11 +17,22 @@ int main() {
     svg.addAttribute("height", "600");
     svg.addAttribute("width", "800");
 
-    
+    double x = 100; // valor inicial do primeiro circulo
+    double y = 100;
+
+    string x_prev = "";
+    string y_prev = "";
     for (int i = 0; i < NUM_CIRCLES; i++) {
-        Circle cir = Circle(300.0, 0, RAD);
+        Circle cir = Circle(x, y, RAD);
+        x += (4 * RAD);
+        y += (4 * RAD);
         string cx = to_string(cir.getCenter().getX());
         string cy = to_string(cir.getCenter().getY());
+
+        if (i % 2 != 0) {
+            x_prev = cx;
+            y_prev = cy;
+        }
         string r = to_string(cir.getRadius());
 
         Tag circle = Tag("circle");
@@ -32,12 +43,20 @@ int main() {
         circle.addAttribute("stroke-width", "3");
         circle.addAttribute("fill", "red");
         svg.addTag(circle);
+
+        if (i % 2 == 0) {
+            Tag line = Tag("line");
+            line.addAttribute("x1", x_prev);
+            line.addAttribute("y1", y_prev);
+            line.addAttribute("x2", cx);
+            line.addAttribute("y2", cy);
+            line.addAttribute("style", "stroke:rgb(255,0,0);stroke-width:2");
+            svg.addTag(line);
+        }
     }
 
     file << svg.toString() << endl;
 
     file.close();
-
-    cout << svg.toString() << endl;
     return 0;
 }
