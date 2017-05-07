518ab5718f3535862baf3b8a838a1b50ef4391d1
Joao Bolsson <joaovictorbolsson@gmail.com>
2017-05-07 18:20:24 -0300

    iniciando t5

---
+++ t5/Activity.cpp
@@ -0,0 +1,42 @@
+/**
+ * Class that defines an activity.
+ *
+ * @author João Bolsson (jvmarques@inf.ufsm.br)
+ * @since 2017, 07 May.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+class Activity {
+private:
+    string name, player;
+    int scores;
+
+public:
+    /**
+     * Default construct.
+     * @param name Activity name.
+     * @param player Activity player.
+     */
+    Activity(string name, string player) {
+        this->name = name;
+        this->player = player;
+    }
+
+    string getPlayer() {
+        return player;
+    }
+
+    /**
+     * @return Activity name.
+     */
+    string getName() {
+        return name;
+    }
+
+    bool operator<(Activity &act) {
+        return this->scores > act.scores;
+    }
+};
---
+++ t5/Player.cpp
@@ -0,0 +1,54 @@
+/**
+ * Class that defines a player.
+ *
+ * @author João Bolsson (jvmarques@inf.ufsm.br)
+ * @since 2017, 07 May.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+class Player {
+private:
+    string name;
+    int scores;
+
+public:
+    /**
+     * Default construct.
+     * @param name Player name.
+     * @param scores Initial scores.
+     */
+    Player(string name, int scores) {
+        this->name = name;
+        this->scores = scores;
+    }
+
+    /**
+     * @return Player name.
+     */
+    string getName() {
+        return name;
+    }
+
+    /**
+     * Add scores.
+     * @param scores Scores to add.
+     */
+    void sumScores(int scores) {
+        this->scores += scores;
+    }
+
+    /**
+     * @return Player scores.
+     */
+    int getScores() {
+        return this->scores;
+    }
+
+    bool operator<(Player &player) {
+        return this->name.compare(player.getName()) < 0;
+    }
+
+};
