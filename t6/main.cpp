b847b51bfd245514215400e054322c099c9fb0b8
Joao Bolsson <joaovictorbolsson@gmail.com>
2017-05-21 19:59:16 -0300

    adiciona instrucao para gerar executavel

--- t6/main.cpp
+++ t6/main.cpp
@@ -1,3 +1,12 @@
+/**
+ * Main file.
+ *
+ * Generate a executable with:
+ * g++ -std=c++11 main.cpp Shape.cpp Rectangle.cpp Circle.cpp Triangle.cpp Cookie.cpp Util.cpp Pack.cpp -o main
+ *
+ * @author João Bolsson (jvmarques@inf.ufsm.br)
+ * @since 2017, 21 May.
+ */
 #include "Pack.cpp"
 
 using namespace std;
